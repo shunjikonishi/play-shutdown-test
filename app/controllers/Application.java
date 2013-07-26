c8ab87e22f08cbb5c69d71a2dae391ac6dffdcff
k-shunji <k-shunji@flect.co.jp>
2013-07-26 13:14:30 +0900

    Add HostName

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,6 +4,7 @@ import play.*;
 import play.mvc.*;
 
 import java.util.*;
+import java.net.InetAddress;
 
 import models.*;
 
@@ -12,24 +13,37 @@ public class Application extends Controller {
 	private static final String HEROKU_APP_NAME = System.getenv("HEROKU_APPLICATION_NAME");
 	private static final String HEROKU_API_KEY  = System.getenv("HEROKU_API_KEY");
 	
+	private static final String HOST;
+	
+	static {
+		String s = null;
+		try {
+			s = InetAddress.getLocalHost().toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		HOST = s;
+	}
+	
 	public static void index() {
 		String appname = HEROKU_APP_NAME;
-		render(appname);
+		String host = HOST;
+		render(host, appname);
 	}
 	
 	public static void ok() {
-		Logger.info("OK");
+		Logger.info("OK, Host=" + HOST + ", Thread=" + Thread.currentThread().getName());
 		renderText("OK");
 	}
 	
 	public static void sleep(int ms) {
-		Logger.info("Start sleeping for " + ms + "ms, Thread=" + Thread.currentThread().getName());
+		Logger.info("Start sleeping for " + ms + "ms, Host=" + HOST + ", Thread=" + Thread.currentThread().getName());
 		try {
 			Thread.sleep(ms);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		Logger.info("End sleeping, Thread=" + Thread.currentThread().getName());
+		Logger.info("End sleeping, Host=" + HOST + ", Thread=" + Thread.currentThread().getName());
 		renderText("Sleep " + ms + "ms");
 	}
 
